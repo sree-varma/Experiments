0cea1c6686de80874f714df6a22fdbc88efce6fd
Sreedevi Varma <sreevarma2003@gmail.com>
2019-05-14 11:20:03 +0100

    Update process.py

--- Images/process.py
+++ Images/process.py
@@ -16,15 +16,21 @@ import gc
 import preprocessing
 import argparse
 
+readme=raw_input("Did you read the readme file :D :P ? ")
+
 parser = argparse.ArgumentParser(description=__doc__)
 
 parser.add_argument('-method', '--pre-method', dest='method', type=str, help='Preprocessing methods to be used! mit,deeptop_17,deeptop_18')
 parser.add_argument('-dataname', '--image-name', dest='dataname', type=str, help='Name of the dataset to be generated!tW,jW,top,qcd,ZZ,Zj,quarks,gluons')
 
+parser.add_argument('-signame', '--signame', dest='signame', type=str, help='Name of the background! tW,top,ZZ')
+parser.add_argument('-bgname', '--background-name', dest='bgname', type=str,  help='Name of the background! jW,qcd,Zj')
+
 parser.add_argument('-pt', '--ptrange', dest='ptrange', type=str, help='pt range! 350-400,500-550,1300-1400,500')
 parser.add_argument('-R', '--radius', dest='R', type=str, help='Radius of the jet! 0.8,1.5')
 
 parser.add_argument('-channels', '--number of color channels of the image', dest='channels', type=float, default=1, help='Number of color channels of the image')
+
 
 parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False,help='Print output on progress running script')#radius=0.8,pixel=33
 
@@ -36,8 +42,12 @@ dataname=args.dataname
 
 ptrange=args.ptrange
 R=args.R
-
+signame=args.signame
+bgname=args.bgname
 channels=int(args.channels)
+
+	
+
 
 r =0.00001
 
@@ -63,26 +73,33 @@ if channels==3:
 image=[]
 mass=[]
 def read_csv(filename,path):
-    df=pd.read_csv(path+filename,dtype=None,delimiter=",")
+    df=pd.read_csv(path+filename,dtype=None,delimiter=",")#,names=['ETA','PHI','PT','CHARGE'])
     gc.collect()
     return df
-for i in range(1,folders+1):
+for i in range(1,21):
 
 	imFile1='run_%002d/jet_image_R_%s.dat'%(i,R)
 	immass1='run_%002d/jet_mass_R_%s.dat'%(i,R)
+	
 	if os.path.exists(pathim+imFile1) and os.path.exists(pathim+immass1) :
 		imFile1='run_%002d/jet_image_R_%s.dat'%(i,R)
 		immass1='run_%002d/jet_mass_R_%s.dat'%(i,R)
-		
+		print pathim+imFile1
+
 	else:
 		imFile1='run_%002d/jet_image.dat'%i
 		immass1='run_%002d/jet_mass.dat'%i
-			
+		print pathim+imFile1
 	
 	df=pd.DataFrame([[0,0,0,0]],columns=['ETA','PHI','PT','CHARGE'])
+	df_m=pd.DataFrame([[0]],columns=['MASS'])	
+
 	df_im=read_csv(imFile1,pathim)
 
 	df_mass=pd.read_csv(pathim+immass1,sep='\t',names=["MASS"])
+	df_mass=df_m.append(df_mass,ignore_index=True)
+
+	#df_im,df_mass=preprocessing.data(df_im,df_mass)
 
 	df1=df.append(df_im.iloc[[-1]],ignore_index=True)
 	df_im=df1.append(df_im,ignore_index=True)
@@ -94,7 +111,7 @@ for i in range(1,folders+1):
 	g= df_im.loc[df_im['ETA']=='@@@'] 
 	gi = g.index
 	a1=np.size(gi)
-
+	print a1
 	im_n1,mass_n1=preprocessing.image_array(gi,df_im,df_mass,color=color,method=method)
 
 
@@ -107,8 +124,8 @@ for i in range(1,folders+1):
 
 image = np.concatenate([image[i] for i in range(len(image))],axis=2)
 mass=np.concatenate([mass[i] for i in range(len(mass))])
-np.savez_compressed(path+"train_image_"+dataname+"_"+ptrange+"_"+method+".npz",image) 
-np.savez_compressed(path+"train_mass_"+dataname+"_"+ptrange+"_"+method+".npz",mass)
+np.savez_compressed(path+signame+"_"+bgname+'/'+ptrange+'/'+"train_image_"+dataname+"_"+ptrange+"_"+R+"_"+method+".npz",image) 
+np.savez_compressed(path+signame+"_"+bgname+'/'+ptrange+'/'+"train_mass_"+dataname+"_"+ptrange+"_"+R+"_"+method+".npz",mass)
 
 
 
