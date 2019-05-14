edb1756849427af46b1c9a676cf476f4bb54c439
Sreedevi Varma <sreevarma2003@gmail.com>
2019-05-14 11:18:54 +0100

    Update preprocessing.py

--- Images/preprocessing.py
+++ Images/preprocessing.py
@@ -19,6 +19,8 @@ import  matplotlib.transforms as transforms
 from cmath import rect, phase
 import skimage.transform as sk
 import scipy.misc
+from pyjet import cluster
+
 
 
 radius=input("Radius of the jet: ")
@@ -29,6 +31,67 @@ delta = (2*radius)/pixel
 boxmax=radius
 r = 0.00001
 
+
+min_pt=500
+max_pt=650
+"""
+def mapangle_0to2PI(angle):
+     #Map an angle into the range [0, 2PI).
+        #https://rivet.hepforge.org/code/dev/namespaceRivet.html#a261c71a1b95aa3028b685becb0e8003f
+    
+    rtn = np.fmod(angle, 2*np.pi);
+    assert rtn >= -2*np.pi and rtn <= 2*np.pi
+    if iszero(rtn):
+        return 0.
+    if rtn < 0:
+        rtn += 2*np.pi;
+    if iszero(rtn - 2*np.pi):
+        rtn = 0
+    assert rtn >= 0 and rtn < 2*np.pi
+    return rtn
+"""
+def mapangle_MPItoPI(angle):
+    """ Map an angle into the range [0, 2PI).
+        https://rivet.hepforge.org/code/dev/namespaceRivet.html#a261c71a1b95aa3028b685becb0e8003f
+    """
+    rtn = np.fmod(angle, 2*np.pi)
+    assert rtn >= 0 and rtn <= 2*np.pi
+    if rtn==0:
+        return 0.
+    if rtn > np.pi:
+        rtn =rtn- 2*np.pi
+    if rtn<= -np.pi:
+        rtn = rtn+2*np.pi
+    assert rtn > -np.pi and rtn <=np.pi
+    return rtn
+
+
+
+def data(df_image,mass_jet):
+	indx=df_image.loc[df_image['ETA']=='@@@']
+	mass_jet = np.asarray(mass_jet)
+	index_array=indx.index
+	indices=[]
+        mass =[]# np.zeros((np.size(index_array),1),dtype=float)
+        #mass_jet=np.array(mass_jet,dtype=float)
+	for index,j in enumerate(index_array):
+		if index_array[index-1]+1<index_array[index]:
+			image= df_image.iloc[index_array[index-1]+1:index_array[index],:]
+			jet_stats=df_image.iloc[index_array[index-1]+1]
+			#print jet_stats.PT
+			if (float(jet_stats.PT)>(min_pt)) and (float(jet_stats.PT)<(max_pt)):#float(jet_stats.PT)>=min_PT:# and float(jet_stats.PT)<max_PT:
+				#if float(jet_stats.PT)<max_PT:
+				indices.append([index_array[index-1],index_array[index]])
+				#print mass_jet[index]
+				mass.append(mass_jet[index])
+	indexed_jets = map(lambda x: df_image.iloc[range(*x)], indices)
+	# put together the high pT jets into a refined single dataset and reindex it
+	data = pd.concat(indexed_jets, ignore_index=True)
+	mass = np.asarray(mass)
+	l=[data,mass]
+	#q.put(l)
+	return data,mass
+
 def centering(eta,phi,pT):
 	"""
 	ETA
@@ -37,7 +100,9 @@ def centering(eta,phi,pT):
 	eta =np.array(eta)
         eta =eta.reshape((np.size(eta),)) 
 	mean_eta=np.ma.average(eta,axis=0,weights=pT)
-       	eta_new =(eta- mean_eta)
+       	eta_new =(eta-mean_eta)
+	#print eta,eta_new
+	
  
 	"""
 	PHI
@@ -45,22 +110,35 @@ def centering(eta,phi,pT):
         """
         phi =np.array(phi) 
         phi =phi.reshape((np.size(phi),))
- 	mean_phi = np.ma.average(phi,axis=0,weights=pT)
-	phi_new =(phi-mean_phi)		
+	#for i in range(phi.shape[0]):
+	#	k=mapangle_MPItoPI(phi[i])
+	#	phi[i]=k
+	mean_phi = np.ma.average(phi,axis=0,weights=pT)
+	phi_new =(phi-mean_phi)	
 	
-	if (np.sum(np.abs(phi_new)))>boxmax*np.shape(phi_new)[0]:	
+	
+	
+	if (np.sum(abs(phi_new)))>boxmax*np.shape(phi_new)[0]:		
+		
 		phi_pi = np.zeros((np.shape(phi)[0],))
+		
 		for m in range(np.shape(phi)[0]):
 			if (phi[m])>=3.13359:
 				if (phi[m])<=6.28319:
-					phi[m]=phi[m]-2*np.pi
+					phi_pi[m]=phi[m]-2*np.pi
 	
-		mean_phi_new =np.ma.average(phi,axis=0,weights=pT)
-		phi_new=phi-mean_phi_new	
-	
+
+		
+		#phi =phi.reshape((np.size(phi),))
+
+		mean_phi_new =np.ma.average(phi_pi,axis=0,weights=pT)
+		phi_new=phi_pi-mean_phi_new	
+		#print phi_new
+		#exit()	
+
 	return eta_new,phi_new
 
-def principal_axis(image):
+def principal_axis_1(image):
 	width, height = image.shape
 	pix_coords = np.array([[i, j] for i in range(-width+1, width, 2) for j in range(-height+1, height, 2)])
 	covX = np.cov(pix_coords, aweights=np.reshape(image, (width*height)),rowvar=0, bias=1)
@@ -82,6 +160,53 @@ def principal_axis(image):
 
 
 
+def principal_axis(eta,phi_new,pT):
+	image=np.zeros((eta.shape[0],2),dtype=float)
+	image[:,0]=eta
+	image[:,1]=phi_new
+
+	coords=image
+	cov=np.cov(coords.T,aweights=pT)
+	val,vec=np.linalg.eigh(cov)
+	sort_indices = np.argsort(val)[::-1]
+	x_v1, y_v1 = vec[0, sort_indices[0]],vec[1, sort_indices[0]]
+	prin_ax= np.asarray([x_v1,y_v1])
+	size,_ = np.shape(image)
+	theta =np.arctan2(y_v1,x_v1 )
+	if theta<np.pi/2 and theta>=0:
+		theta1 = (np.pi/2)-(theta)
+		image[:,0]=eta*np.cos(theta1)-phi_new*np.sin(theta1)
+		image[:,1]=eta*np.sin(theta1)+phi_new*np.cos(theta1)
+
+	
+	if theta>np.pi/2 and theta<=np.pi:
+		theta1 =(np.pi/2)-(np.pi-theta)
+		image[:,0]=eta*np.cos(theta1)+phi_new*np.sin(theta1)
+		image[:,1]=-eta*np.sin(theta1)+phi_new*np.cos(theta1)
+
+	if theta>-np.pi and theta<=-np.pi/2:
+		theta1 = (np.pi/2)-(np.pi+theta)
+		image[:,0]=eta*np.cos(theta1)-phi_new*np.sin(theta1)
+		image[:,1]=eta*np.sin(theta1)+phi_new*np.cos(theta1)
+
+	
+	if theta <0 and theta>-np.pi/2:
+		theta1 = (-np.pi/2)-(theta)#(-np.pi/2)-(theta) #CLOCKWISE
+		image[:,0]=eta*np.cos(theta1)-phi_new*np.sin(theta1)
+		image[:,1]=eta*np.sin(theta1)+phi_new*np.cos(theta1)	
+
+	if theta==np.pi/2:
+		theta1=theta
+		image[:,0]=eta*np.cos(theta1)-phi_new*np.sin(theta1)
+		image[:,1]=eta*np.sin(theta1)+phi_new*np.cos(theta1)	
+
+	maxpt=np.argmax(pT)
+	if (image[maxpt,1])<0:	
+		image[:,1] = -1*(image[:,1])
+	if (image[maxpt,0])<0:
+		image[:,0] = -1*(image[:,0])
+		
+	return image[:,0],image[:,1]
 
 
 def pixelize(eta_new,phi_new,pT,c2,c3,img_size,number,boxmax,delta,channels=1):
@@ -220,7 +345,7 @@ def image_array(index_array,df_image,mass_jet,color='grayscale',method='mit'):
     	#pT_jet=pT_jet[:,0]    
 
     	zero =np.zeros(1,dtype=float)
-    	mass_jet=np.concatenate((zero,mass_jet),axis=0)
+    	#mass_jet=np.concatenate((zero,mass_jet),axis=0)
     	#pT_jet=np.concatenate((zero,pT_jet),axis=0)
     
     	for index,j in enumerate(index_array):      
@@ -258,10 +383,13 @@ def image_array(index_array,df_image,mass_jet,color='grayscale',method='mit'):
 			
 
 		    	if color == 'grayscale':
+				if method =='deeptop_18':
+					eta_new,phi_new=principal_axis(eta_new,phi_new,pT)
+
+
 	        		channel1_array[:,:,index]=pixelize(eta_new,phi_new,pT,pT,pT,img_size=(pixel,pixel),number=number,boxmax=boxmax,delta=delta,channels=1)
 				
-				if method =='deeptop_18':
-					channel1_array[:,:,index]=principal_axis(channel1_array[:,:,index])
+
 				if method=='deeptop_17':
 
 					channel1_array[:,:,index],maxima=shift(channel1_array[:,:,index],img_size=(pixel,pixel),number=number,max_number=3)	
@@ -269,10 +397,11 @@ def image_array(index_array,df_image,mass_jet,color='grayscale',method='mit'):
 					channel1_array[:,:,index]=flip(channel1_array[:,:,index],img_size=(pixel,pixel),number=number,max_number=3,maxima=maxima)
 
 				if np.sum(channel1_array[:,:,index])==0.0 or np.sum(channel1_array[:,:,index])=='nan' :
-					print index,mass_jet[index]   
+					print index,mass_jet[index]
+					#exit() 
 				if np.sum(channel1_array[:,:,index])!=0.0:
-					if method == 'mit' or method== 'deeptop_18':
-						channel1_array[:,:,index]=normalize(channel1_array[:,:,index])
+					#if method == 'mit' or method== 'deeptop_18':
+					channel1_array[:,:,index]=normalize(channel1_array[:,:,index])
 					mass[index]= mass_jet[index]
 					#jpT[index]= pT_jet[index]
 					
@@ -288,7 +417,7 @@ def image_array(index_array,df_image,mass_jet,color='grayscale',method='mit'):
 					print index,mass_jet[index]   
 
 				if np.sum(channel1_array[:,:,index])!=0.0 and np.sum(channel1_array[:,:,index])!=0.0 and np.sum(channel1_array[:,:,index])!=0.0:
-				        
+				        print "normalization"
 					channel1_array[:,:,index]=normalize(channel1_array[:,:,index])
 					#channel3_array[:,:,index]=normalize(channel2_array[:,:,index])
 					#channel2_array[:,:,index]=normalize(channel3_array[:,:,index])
@@ -365,14 +494,13 @@ def plotting(index_array,image_array,image_name,train_path,channels=1):#,val_pat
     	for index in range(index_array): 
 		image=image_array[:,:,index]
                 imfile ='_%002d.jpeg'%index
-		scipy.misc.imsave(os.path.join(train_path,image_name+imfile),image.T)
+		scipy.misc.imsave(os.path.join(train_path,image_name+imfile),image.T*(256./np.amax(image)))
 		
     if channels==3:   
     	for index in range(index_array): 
 		image=image_array[:,:,:,index]
                 imfile ='_%002d.jpeg'%index
-		scipy.misc.imsave(os.path.join(train_path,image_name+imfile),image.T)
-
+		scipy.misc.imsave(os.path.join(train_path,image_name+imfile),image.T*(256./np.amax(image)))
 
 
 
